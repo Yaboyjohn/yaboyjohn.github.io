f3907ce861bc11740c682e229195318bb9446017
Yaboyjohn <johnang117@gmail.com>
2016-02-20 01:22:08 -0800

    no more keypress

--- script.js
+++ script.js
@@ -165,52 +165,6 @@ var click = 0
     }
   }
 });
-var click = 0
-$(document).keypress(function(event){
-  if (event.which === 109){
-    click += 1
-    if (click === 1){
-      $('#menu-icon').rotate({
-        duration: 400,
-        angle: 0,
-        animateTo: 90
-    })
-    }
-    else
-    click = 0
-        $('#menu-icon').rotate({
-          duration:400,
-          angle: 90,
-          animateTo:180
-        })
-  }
-});
-
-var menuopen = 0
-  $(document).keypress(function(event){
-    if(event.which === 109){
-      menuopen += 1
-      if (menuopen === 1){
-      $('#menu-container').animate({
-        left: "-215px"
-      }, 200);
-      $('.transparent').addClass('menu-open');
-      $('.slideout-container').animate({
-        right: "0px"
-      }, 200);
-      }
-      else {
-        menuopen = 0
-        $('#menu-container').animate({
-          left: "0px"
-        }, 200);
-        $('.transparent').removeClass('menu-open')
-        $('.slideout-container').animate({
-          right: "-300px"
-        }, 200);
-        }
-    }
-  });
     var menuopen = 0
     $("#menu-container").click(function() {
       menuopen += 1
